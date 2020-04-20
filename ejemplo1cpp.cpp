263d98b77c3e0ac51674e21734bcda963c3e36de
marco-on <marco.ortiz@escuelaing.edu.co>
2020-04-20 13:30:22 +0000

    se añade namesapace std

--- ejemplo1cpp.cpp
+++ ejemplo1cpp.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-//using namespace std;
+using namespace std;
 
 int main (int argc, char *argv[])
 {
-	std::cout << "la cantidad de argumentos de entrada son: " << argc << std::endl;
+	cout << "la cantidad de argumentos de entrada son: " << argc << endl;
 
 	return 0;
 }
